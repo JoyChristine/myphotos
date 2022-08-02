88551839c19a2f404ee0a656319ab752d76e614b
Joy Christine <joychristin2@gmail.com>
2022-08-02 23:30:25 +0300

    switch between tabs

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('.tabs-menu a').click(function(event) {
+      event.preventDefault();
+      
+      // Toggle active class on tab buttons
+      $(this).parent().addClass("current");
+      $(this).parent().siblings().removeClass("current");
+      
+      // display only active tab content
+      var activeTab = $(this).attr("href");
+      $('.tab-content').not(activeTab).css("display","none");
+      $(activeTab).fadeIn();
+      
+    });
+    
+  });
