59d0eef67b051117f4bf8daa4accc1a2a29b4890
Joy Christine <joychristin2@gmail.com>
2022-08-02 21:12:27 +0300

    get all images

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import  * 
 # Create your views here.
 def welcome(request):
-    return render(request, 'all/index.html')
+    images = image.objects.all()
+    if request.method == 'GET':
+        context = {'images': images}
+        return render(request, 'all/index.html',context)
