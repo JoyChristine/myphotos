950c95dc688f09c907e682e8fe9c6f909b04ca55
Joy Christine <joychristin2@gmail.com>
2022-08-02 21:11:51 +0300

    add class image'

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+class image(models.Model):
+    name = models.TextField()
+    image = CloudinaryField('image',blank=True,null=True)
+    description = models.TextField()
+    location = models.TextField()
